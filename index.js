0e8ed8cfc9fa72f198d995bbb5ca1b466bd42008
nintendaii <danielkarshai8@gmail.com>
2019-07-03 16:08:36 +0300

    appointments - added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var jsonParser = bodyParser.json({ extended: false });
 var mongoose = require("mongoose");
 var uristring = "mongodb://localhost:27017/appointus";
 var Client = require("./models/clients");
+var Appointment = require("./models/appointments");
 
 mongoose.connect(uristring, { useNewUrlParser: true }, function(err) {
   if (err) {
@@ -28,6 +29,27 @@ app.post("/clients", jsonParser, function(req, res) {
   clientToAdd.save(function(err, client) {
     if (err) console.log("Error on save!");
     res.send(client);
+  });
+});
+
+app.get("/appointments", jsonParser, function(req, res) {
+  Appointment.find({ date: req.body.date })
+    .populate("client")
+    .exec(function(err, client) {
+      if (err) console.error(err);
+      res.send(client);
+    });
+});
+
+app.post("/appointments", jsonParser, function(req, res) {
+  var appointmentToAdd = new Appointment({
+    date: req.body.date,
+    time: req.body.time,
+    client: req.body.client
+  });
+  appointmentToAdd.save(function(err, appointment) {
+    if (err) console.log(err);
+    res.send(appointment);
   });
 });
 
