f6948474dd1a0327c16b14e3115e847c1868b9cb
PronLover <kolyab1956@gmail.com>
2019-07-03 15:57:51 +0300

    clients - put method created

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json({ extended: false });
-var mongoose = require("mongoose");
-var uristring = "mongodb://localhost:27017/appointus";
-var Client = require("./models/clients");
+var mongoose = require('mongoose');
+var uristring = 'mongodb://localhost:27017/appointus';
+var Client = require('./models/clients');
 
 mongoose.connect(uristring, { useNewUrlParser: true }, function(err) {
-  if (err) {
-    console.log(err);
-  }
+	if (err) {
+		console.log(err);
+	}
+});
+app.put('/clients/:id', jsonParser, function(req, res) {
+	Client.findOneAndUpdate(
+		req.params.id,
+		{
+			$set: {
+				first_name: req.body.new_first_name,
+				last_name: req.body.new_last_name,
+				phone: req.body.new_phone
+			}
+		},
+		{ new: true },
+		function(err, doc) {
+			if (err) console.log(err);
+			res.send(doc);
+		}
+	);
+});
+app.get('/clients', jsonParser, function(req, res) {
+	Client.find(function(err, clients) {
+		if (err) return console.error(err);
+		res.send(clients);
+	});
 });
 
-app.get("/clients", jsonParser, function(req, res) {
-  Client.find(function(err, clients) {
-    if (err) return console.error(err);
-    res.send(clients);
-  });
-});
-
-app.post("/clients", jsonParser, function(req, res) {
-  var clientToAdd = new Client({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    phone: req.body.phone
-  });
-  clientToAdd.save(function(err, client) {
-    if (err) console.log("Error on save!");
-    res.send(client);
-  });
+app.post('/clients', jsonParser, function(req, res) {
+	var clientToAdd = new Client({
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		phone: req.body.phone
+	});
+	clientToAdd.save(function(err, client) {
+		if (err) console.log('Error on save!');
+		res.send(client);
+	});
 });
 
 app.listen(3000);
--- models/clients.js
+++ models/clients.js
@@ -1,10 +1,10 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var clientSchema = new Schema({
-  first_name: { type: String, trim: true },
-  last_name: { type: String, trim: true },
-  phone: { type: String, trim: true }
+	first_name: { type: String, trim: true },
+	last_name: { type: String, trim: true },
+	phone: { type: String, trim: true }
 });
 
-module.exports = mongoose.model("clients", clientSchema);
+module.exports = mongoose.model('clients', clientSchema);
