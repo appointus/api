c3087a2d7510ebac664d060c9137de94382f6608
nintendaii <danielkarshai8@gmail.com>
2019-07-05 11:00:33 +0300

    config file added

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var config = require('./config');
 
-var uristring = 'mongodb://localhost:27017/appointus';
 var mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect(uristring, { useNewUrlParser: true }, function(err) {
+mongoose.connect(config.mongoURI, { useNewUrlParser: true }, function(err) {
   if (err) {
     console.log(err);
   }
@@ -16,4 +16,4 @@ app.use(bodyParser.json());
 app.use(require('./routes/clients'));
 app.use(require('./routes/appointments'));
 
-app.listen(3000);
+app.listen(config.port);
