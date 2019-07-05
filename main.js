35b86dd8d458314fccaffa0124ae537115d0fdb3
nintendaii <danielkarshai8@gmail.com>
2019-07-05 21:26:38 +0300

    access headers added

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ var config = require('./config');
 
 var mongoose = require('mongoose');
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.connect(config.mongoURI, { useNewUrlParser: true }, function(err) {
