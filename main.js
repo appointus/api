16ea9e0e31e3239e38592c9fc2551f26586569b7
Haikhell <iwilovik@gmail.com>
2019-07-08 10:41:24 +0300

    turboSms util

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cron = require('./crons/smsTomorrow');
+var crons = require('./crons/smsWeek');
 
 var uristring = 'mongodb://localhost:27017/appointus';
 var mongoose = require('mongoose');
@@ -21,3 +22,4 @@ app.use(require('./routes/appointments'));
 app.listen(3000);
 
 cron.runSMS();
+crons.runSMSWeek();
