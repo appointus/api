036d883daee60b74fceef3efa707b5149ce5ac33
Daniel <danielkarshai8@gmail.com>
2021-05-05 22:52:21 +0300

    uri fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.port || 3000,
-    mongoURI: "mongodb+srv://admin:thisiscoolpassword@cluster0.f2pgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI: ""
 };
