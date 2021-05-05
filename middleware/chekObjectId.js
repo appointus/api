42e992043e53d9561d6e12315e99a9472a691c82
Ivan Haikhel <ivanhaikhell@gmail.com>
2021-05-05 18:02:35 +0300

    check

--- middleware/chekObjectId.js
+++ middleware/chekObjectId.js
@@ -1,24 +1,24 @@
 const router = require('express').Router();
 
 router.put('/clients/:id',(req,res,next)=>{
-    chek(req?.params?.id,res,next)
+    check(req?.params?.id,res,next)
 })   
  
 router.put('/clients/:id/activate', function(req, res,next) {
-    chek(req?.params?.id,res,next)
+    check(req?.params?.id,res,next)
 });
 router.put('/clients/:id/deactivate', function(req, res,next) {
-    chek(req?.params?.id,res,next)
+    check(req?.params?.id,res,next)
 });
 router.put('/appointments/:id', function(req, res,next) {
-    chek(req?.params?.id,res,next)
+    check(req?.params?.id,res,next)
 
 })
 router.delete('/appointments/:id/delete', function(req, res,next) {
-    chek(req?.params?.id,res,next)
+    check(req?.params?.id,res,next)
 });
 
-function chek(id,res,next) {
+function check(id,res,next) {
     if (id.match(/^[0-9a-fA-F]{24}$/)) {
         next()
     }
